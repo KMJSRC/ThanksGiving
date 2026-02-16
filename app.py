71e91a8fd857aee95580ebc158497bb4d76e4149
KMJSRC <47475366+KMJSRC@users.noreply.github.com>
2026-02-16 17:12:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def get_font(size, bold=False):
 
 def create_card():
     card_width = 1200
-    card_height = 1800
+    card_height = 1750
 
     # Light background (soft decoration)
     card = Image.new("RGB", (card_width, card_height), (248, 248, 255))
@@ -175,6 +175,7 @@ def create_card():
 if st.button("Generate Thanksgiving Card"):
     card_image = create_card()
     st.image(card_image, caption="Generated Greeting Card", use_column_width=True)
+
 
 
 
